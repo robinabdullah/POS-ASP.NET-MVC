bc47b9e83528cb1034a3e084a6f1ade2c995e93d
robinabdullah <robinabdullah>
2017-09-11 00:35:31 +0600

    f

--- POS ASP.NET MVC/Models/Validation Annotations.cs
+++ POS ASP.NET MVC/Models/Validation Annotations.cs
@@ -6,6 +6,87 @@ using System.Web;
 
 namespace POSEntity
 {
+    #region Product Validation Annotation
+
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product
+    {
+
+    }
+
+    public class ProductMetaData
+    {
+        [Key]
+        public string ID { get; set; }
+        [Required]
+        public string Type { get; set; }
+        [Required]
+        public string Model { get; set; }
+        [Required]
+        public Nullable<int> Quantity_Available { get; set; }
+        [Required]
+        public Nullable<int> Quantity_Sold { get; set; }
+        [Required]
+        public Nullable<int> Unit_Price { get; set; }
+        [Required]
+        public Nullable<int> Selling_Price { get; set; }
+        [Required]
+        public Nullable<System.DateTime> Date_Updated { get; set; }
+        [Required]
+        public string Unique_Barcode { get; set; }
+    }
+    #endregion
+
+    #region Sale Validation Annotation
+
+    [MetadataType(typeof(SaleMetaData))]
+    public partial class Sale
+    {
+
+    }
+
+    public class SaleMetaData
+    {
+        [Key]
+        public int Invoice_ID { get; set; }
+        public Nullable<System.DateTime> Date { get; set; }
+
+    }
+    #endregion
+
+    #region Barcode Validation Annotation
+
+    [MetadataType(typeof(BarcodeMetaData))]
+    public partial class Barcode
+    {
+
+    }
+
+    public class BarcodeMetaData
+    {
+        [Key]
+        public int Invoice_ID { get; set; }
+        [Required]
+        public Nullable<System.DateTime> Date { get; set; }
+
+    }
+    #endregion
+
+    #region Gift Validation Annotation
+
+    [MetadataType(typeof(GiftMetaData))]
+    public partial class Gift
+    {
+
+    }
+
+    public class GiftMetaData
+    {
+        [Key]
+        public string Barcode { get; set; }
+
+    }
+    #endregion
 
     #region Address Validation Annotation
     [MetadataType(typeof(AddressMetaData))]
@@ -26,15 +107,9 @@ namespace POSEntity
         public string Country { get; set; }
         [Required]
         public string Phone1 { get; set; }
-        public string Phone2 { get; set; }
-        public string Fax { get; set; }
-        public string Website { get; set; }
     }
     #endregion
 
-    #region Address Validation Annotation
-
-
-    #endregion
+    
 
 }
